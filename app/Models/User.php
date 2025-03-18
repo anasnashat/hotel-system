dd59af1f0327a5c3713ee6ae7002245a098b40bf
anas nashat <aanasht@gmail.com>
2025-03-18 02:48:09 +0200

    feat(User): add soft deletes and user relations
    - Add SoftDeletes trait to User model
    - Update fillable attributes
    - Add profile, approvedUsers, createdFloors, createdRooms, and reservations relations
    - Simplify hidden attributes array

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
@@ -11,7 +12,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -19,21 +20,39 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
-        'manager_id',
+        'name', 'email', 'password', 'avatar_image', 'gender',  'last_login_at'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token'];
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+
+
+    public function approvedUsers()
+    {
+        return $this->hasMany(UserProfile::class, 'approved_by');
+    }
+
+    public function createdFloors()
+    {
+        return $this->hasMany(Floor::class, 'created_by');
+    }
+
+    public function createdRooms()
+    {
+        return $this->hasMany(Room::class, 'created_by');
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class, 'client_id');
+    }
+
+
 
     /**
      * Get the attributes that should be cast.
@@ -47,4 +66,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+
+
 }
