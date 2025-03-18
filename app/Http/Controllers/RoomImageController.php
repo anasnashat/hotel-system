f50c748e8ffa3769542db405d3edda1d04359fb1
anas nashat <aanasht@gmail.com>
2025-03-18 02:50:19 +0200

    feat(app): add request classes and controllers for Room, Floor, UserProfile, and Reservation

---
+++ app/Http/Controllers/RoomImageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRoomImageRequest;
+use App\Http\Requests\UpdateRoomImageRequest;
+use App\Models\RoomImage;
+
+class RoomImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRoomImageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RoomImage $roomImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RoomImage $roomImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRoomImageRequest $request, RoomImage $roomImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RoomImage $roomImage)
+    {
+        //
+    }
+}
