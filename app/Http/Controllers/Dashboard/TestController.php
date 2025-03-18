441d5f8804f2f5c3e1ca00eb43a93da522d3b72c
anas nashat <aanasht@gmail.com>
2025-03-18 02:47:56 +0200

    refactor(dashboard): update TestController to include user approval functionality
    - Add User and UserProfile models to TestController- Implement logic to retrieve and update user approval status
    - Add comments for potential future improvements

--- app/Http/Controllers/Dashboard/TestController.php
+++ app/Http/Controllers/Dashboard/TestController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\UserProfile;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
     public function index()
     {
-        return inertia('Test/index');
+//        $requests = UserProfile::where('approved_by', null)->get();
+//        $user = $requests[0];
+//
+//        $user->update(['approved_by' => 5]);
+//        dd($user);
+//        dd($requests);
+
+        $userId = 5;
+        $user = User::with('approvedUsers.user')->where('id', $userId)->first();
+
+        dd($user);
     }
 }
