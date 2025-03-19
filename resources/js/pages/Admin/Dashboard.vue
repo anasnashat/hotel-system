a5ff4e8716ea9d5ab9a0a51ae091108b366625db
Nadaehab13 <ehabnada875@gmail.com>
2025-03-19 02:20:32 +0200

    login with inertia

---
+++ resources/js/pages/Admin/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome, Admin!</p>
+        <h2>Managers</h2>
+        <ul>
+            <li v-for="manager in managers" :key="manager.id">{{ manager.name }} ({{ manager.email }})</li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+defineProps({ managers: Array });
+</script>
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
+Route::get('/login', [AuthenticatedSessionController::class, 'create'])
+    ->name('login');
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']) 
+ ->middleware('auth');
 
 Route::get('test/', [App\Http\Controllers\Dashboard\TestController::class, 'index']);
-
-
-
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
