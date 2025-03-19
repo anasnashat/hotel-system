af7b713df354e04bb336a5370078c827bb2473b0
anas nashat <aanasht@gmail.com>
2025-03-19 18:06:57 +0200

    feat(routes): update dashboard route middleware to include role-based access control

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,12 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
+
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified', 'role:admin|manager|receptionist'])->name('dashboard');
+
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
