dd389b78d9c572d384d13318a7bdcb25649b0586
anas nashat <aanasht@gmail.com>
2025-03-22 15:22:41 +0200

    feat(Room.php): implement media handling and auto-generate unique floor number

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -4,12 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use phpDocumentor\Reflection\Types\This;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Room extends Model
+class Room extends Model implements HasMedia
 {
     use SoftDeletes;
+    use InteractsWithMedia;
 
-    protected $fillable = ['number', 'capacity', 'price', 'description', 'floor_id', 'created_by'];
+
+    // auth()->id
+    protected $fillable = ['capacity', 'price', 'description', 'floor_id'];
 
     public function floor()
     {
@@ -29,6 +35,40 @@ class Room extends Model
     public function reservations()
     {
         return $this->hasMany(Reservation::class);
+    }
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('rooms_image')
+            ->useDisk('public')
+            ->useFallbackPath(public_path('images/default.jpg'))
+            ->useFallbackUrl(url('images/default.jpg'));
+    }
+    public function registerMediaConversions($media = null): void
+    {
+        $this->addMediaConversion('thumb')
+            ->width(200)
+            ->height(200)
+            ->sharpen(10);
+    }
+    public function getMediaDirectoryAttribute()
+    {
+        return 'rooms/' . $this->number;
+    }
+
+    protected static function generateUniqueFloorNumber(): int
+    {
+        $maxNumber = Room::max('number');
+        $randomNumber = rand(1, 1000);
+        return $maxNumber ? $maxNumber + $randomNumber : 1000;
+    }
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($floor) {
+            // Automatically set created_by to the authenticated user's ID
+            $floor->created_by = auth()->id();
+            $floor->number =  self::generateUniqueFloorNumber();
+        });
     }
 }
 
