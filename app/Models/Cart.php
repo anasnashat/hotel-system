12fb545e5437ac7c9a204f4daf355b2c0d5852fe
anas nashat <aanasht@gmail.com>
2025-03-25 01:18:50 +0200

    feat: implement favorites and cart functionality with models and controller methods

---
+++ app/Models/Cart.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+class Cart extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cart';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'room_id',
+    ];
+
+    public $timestamps = true;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($cart) {
+            $cart->user_id = auth()->id();
+        });
+    }
+}
