fc92587a630176c2863616201d5fdc9d5735801a
anas nashat <aanasht@gmail.com>
2025-03-25 01:18:55 +0200

    feat: add Favorite model and UserItemsController for managing favorites and cart functionality

---
+++ app/Models/Favorite.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Favorite extends Model
+{
+    use HasFactory;
+
+    protected $table = 'favorites';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'room_id',
+    ];
+
+    public $timestamps = true;
+    public function user(): belongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function room(): belongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
+    public static function boot(): void
+    {
+        static::creating(function ($favorite) {
+            $favorite->user_id = auth()->id;
+        });
+    }
+
+}
