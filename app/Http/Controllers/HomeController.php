22b74d4fcd8178257b52fb88098aa02f531e32ad
anas nashat <aanasht@gmail.com>
2025-03-28 03:22:40 +0200

    feat: add HomeController to handle room data rendering for the home page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Room;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $rooms = Room::with(['media'])
+            ->get()
+            ->map(function ($room) {
+                return [
+                    'id' => $room->id,
+                    'price' => $room->price,
+                    'number' => $room->number,
+                    'is_available' => $room->is_available,
+                    'first_image_url' => $room->getFirstMediaUrl('rooms_image'),
+                ];
+            });
+
+        return Inertia::render('Home', [
+            'rooms' => $rooms,
+        ]);
+    }
+
+}
