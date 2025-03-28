5643a28f27668dcda0a87aa4a9d8c2f94f74758d
anas nashat <aanasht@gmail.com>
2025-03-28 03:21:10 +0200

    feat: create RoomCard component to display a list of rooms

---
+++ resources/js/pages/Home/RoomCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import RoomCard from '@/components/ui/card/RoomCard.vue';
+import { usePage } from '@inertiajs/vue3';
+
+
+const page = usePage();
+const rooms = page.props.rooms
+
+
+</script>
+
+<template>
+    <div class="container mx-auto mt-5">
+        <a href="#rooms"><h2 class="title-text text-center">Rooms</h2></a>
+    </div>
+    <div class="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <RoomCard v-for="room in rooms" :key="room.id" :room="room" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
