10ef6cebfe7d131d18bedc0fc30fd2f69ec7c1d7
anas nashat <aanasht@gmail.com>
2025-03-28 03:20:35 +0200

    feat: pass rooms data to RoomsList component for improved rendering

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,11 +3,23 @@ import NavBarMain from '@/components/NavBarMain.vue';
 import RoomsList from '@/components/ui/card/RoomsList.vue';
 import WelcomeVideo from '@/components/WelcomeVideo.vue';
 import Footer from '@/components/Footer.vue';
+import { usePage } from '@inertiajs/vue3';
+import { computed } from 'vue';
+
+const page = usePage();
+
+const rooms = computed(() => page.props.rooms);
 </script>
+
+
+
+
+
+
 
 <template>
     <NavBarMain/>
     <WelcomeVideo/>
-    <RoomsList/>
+    <RoomsList :rooms="rooms"/>
     <Footer/>
 </template>
